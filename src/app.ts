10b492792a87c44e98cd49fe12130b9747a9a4bf
Goncalo Marques <goncalomarques9@hotmail.com>
2021-01-29 15:45:58 +0000

    project list

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,37 @@ const AutoBind = (
   return adjDescriptor;
 }
 
+//project list
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = <HTMLTemplateElement>document.getElementById('project-list');
+    this.hostElement = <HTMLDivElement>document.getElementById('app');
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = <HTMLElement>importedNode.firstElementChild;
+    this.element.id = `${this.type}-input`;
+
+    this.attach();
+    this.renderContent();
+  }
+  
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+  
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+
+}
+
+//project input
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -59,7 +90,7 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = <HTMLFormElement>importedNode.firstElementChild;
-    // this.element.id = 'user-input';
+    this.element.id = 'user-input';
 
     this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
     this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
@@ -135,3 +166,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
